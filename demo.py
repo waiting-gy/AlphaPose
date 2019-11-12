c43650df6a2a87a5b4e078c2f831ae0ff7f51eb3
waiting-gy <904450727@qq.com>
2019-11-12 09:58:50 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -49,7 +49,7 @@ if __name__ == "__main__":
     sys.stdout.flush()
     det_loader = DetectionLoader(data_loader, batchSize=args.detbatch).start()
     det_processor = DetectionProcessor(det_loader).start()
-    
+
     # Load pose model
     pose_dataset = Mscoco()
     if args.fast_inference:
@@ -83,7 +83,7 @@ if __name__ == "__main__":
             ckpt_time, det_time = getTime(start_time)
             runtime_profile['dt'].append(det_time)
             # Pose Estimation
-            
+
             datalen = inps.size(0)
             leftover = 0
             if (datalen) % batchSize:
@@ -102,7 +102,7 @@ if __name__ == "__main__":
 
             ckpt_time, post_time = getTime(ckpt_time)
             runtime_profile['pn'].append(post_time)
-        
+
         if args.profile:
             # TQDM
             im_names_desc.set_description(
