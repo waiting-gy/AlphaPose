003a1d4633854580d3d2454b04621fcfcf1538f4
waiting-gy <904450727@qq.com>
2019-11-13 16:05:12 +0800

    Update coco.py

--- train_sppe/src/utils/dataset/coco.py
+++ train_sppe/src/utils/dataset/coco.py
@@ -16,7 +16,7 @@ class Mscoco(data.Dataset):
     def __init__(self, train=True, sigma=1,
                  scale_factor=(0.2, 0.3), rot_factor=40, label_type='Gaussian'):
         #self.img_folder = '../data/coco/images'    # root image folders
-        self.img_folder = '/kaggle/working/train_sppe/data/coco/'    # root image folders
+        self.img_folder = '/kaggle/working/train_sppe/data/coco/val2017'    # root image folders
         self.is_train = train           # training set or test set
         self.inputResH = opt.inputResH
         self.inputResW = opt.inputResW
